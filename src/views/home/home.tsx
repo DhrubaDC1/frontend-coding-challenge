e162a25521fa18264dd352f5c946da485aa16c3f
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2024-11-04 02:04:45 +0600

    feat: log the user agent even with js off

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,10 +1,13 @@
 import { createStyledLink } from "@/utils/createStyledLink";
 import React from "react";
+import { NextApiRequest, NextApiResponse } from 'next';
+
 
 const PRODUCTS_LINK_TEXT = "Products";
 const USER_AGENT_LINK_TEXT = "User Agent";
 
-export const Home: React.FC = () => {
+export const Home: React.FC = async () => {
+
   return (
     <div className="flex flex-col items-center justify-center min-h-[100vh]">
       <div className="text-center">
